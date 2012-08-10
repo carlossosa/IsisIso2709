2b6dd52d9761488a3ee2c1f00a46d6bc63b12164
Carlos Sosa <carlitin@gmail.com>
2012-08-10 08:23:03 -0400

    Algunos comentarios

--- import.php
+++ import.php
@@ -1,11 +1,13 @@
 <?php
-
+//Capaz de Procesar 60000 sentencias indivuales
+//en algo menor de 30 segundos
 $count = 0;
-$con = mysql_connect('localhost','root','x34lae2010');
+$con = mysql_connect('localhost','root','');
 mysql_select_db('bnjm_snbp');
 mysql_query("SET NAMES 'utf8'");
 @ini_set('auto_detect_line_endings', true);
 $r = fopen('salida.sql', 'r');
+// @todo: Reemplazar feof por stream_get_line, que emplea almenos un 50% menos de tiempo para leer del archivo
 while ( !feof($r))
 {
     $count++;
